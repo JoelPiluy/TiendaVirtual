de0a0e4e42ca5ae93bb1b295caac4cad835d1549
JoelPiluy <joelpiluy@gmail.com>
2023-11-27 15:49:42 -0400

    Se añadio mas articulos al index.hmtl con sus imagenes y se cambio las variables del archivo carrito.js de var a let

--- carrito.js
+++ carrito.js
@@ -1,25 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var urlParams = new URLSearchParams(window.location.search);
-    var productosString = urlParams.get("productos");
-    var productos = JSON.parse(decodeURIComponent(productosString));
+    let urlParams = new URLSearchParams(window.location.search);
+    let productosString = urlParams.get("productos");
+    let productos = JSON.parse(decodeURIComponent(productosString));
     mostrarProductosComprados(productos);
 });
 
 function mostrarProductosComprados(productos) {
-    var productosCompradosElemento = document.getElementById("productosComprados");
+    let productosCompradosElemento = document.getElementById("productosComprados");
 
     if (productos && productos.length > 0) {
-        var totalCarrito = 0;
+        let totalCarrito = 0;
 
         productos.forEach(function (producto) {
-            var productoElemento = document.createElement("p");
+            let productoElemento = document.createElement("p");
             productoElemento.textContent = producto.nombre + " - $" + producto.precio.toFixed(2);
             productosCompradosElemento.appendChild(productoElemento);
 
             totalCarrito += producto.precio;
         });
 
-        var totalElemento = document.createElement("p");
+        let totalElemento = document.createElement("p");
         totalElemento.textContent = "Total del Carrito: $" + totalCarrito.toFixed(2);
         productosCompradosElemento.appendChild(totalElemento);
     } else {
